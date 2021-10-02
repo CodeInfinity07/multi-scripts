7ac3321eacf8701c246798bb225ddc01d34626c3
zainrk1 <zainrrkk87@gmail.com>
2021-10-02 15:08:56 +0500

    Update Multiple-Client-Futures.py

--- Multiple-Client-Futures.py
+++ Multiple-Client-Futures.py
@@ -6,12 +6,7 @@ Created on Sun Sep 26 15:02:49 2021
 @author: ryzon
 """
 
-import os
 import datetime
-import pandas_ta as pta
-import btalib
-import pandas as pd
-import json
 from binance.client import Client
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 from time import sleep
@@ -28,10 +23,10 @@ ray.init()
 updater = Updater('2028793873:AAFAHKocwFb8aFA5aGVRIAgnL0Tm2ycHhGc', use_context=True)
 
 try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='binance',
-                                         user='root',
-                                         password='zain0980')
+    connection = mysql.connector.connect(host='166.62.25.253',
+                                         database='binance-db',
+                                         user='mitesh_jb',
+                                         password='MITESHmitesh@2')
 except Exception as e:
     print(e)
 
